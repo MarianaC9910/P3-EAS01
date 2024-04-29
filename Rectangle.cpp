923702d6fd0da9f5775b3fff920cdf7aefb5374f
Mariana Cabrera <21152@portalcedi.edu.mx>
2024-04-29 12:54:10 -0600

    cambios

--- Rectangle.cpp
+++ Rectangle.cpp
@@ -12,6 +12,10 @@ Rectangle::Rectangle(Vector2f size)
         - objective: un Vector2f con valores (0, 0).
         - Haz que el cuadrado se pinte de un color.
     */
+   this->shape = RectangleShape(size);
+   this->speed = Vector2f (0.f, 0.f);
+   this-> objective = Vector2f(0.f,0.f);
+   this->shape.setFillColor(Color::Blue);
 }
 
 Rectangle::Rectangle(Vector2f size, Vector2i position)
@@ -24,6 +28,12 @@ Rectangle::Rectangle(Vector2f size, Vector2i position)
         - Haz que el cuadrado se pinte de un color.
         - Haz que el cuadrado esté en la posición position.
     */
+    this->shape = RectangleShape(size); 
+    this->speed = Vector2f(0.f, 0.f);
+    this->objective= Vector2f(position);
+    this->shape.setFillColor(Color::Blue);
+    this->shape.setPosition(Vector2f(position)); 
+    
 }
 
 void Rectangle::update()
@@ -34,10 +44,24 @@ void Rectangle::update()
         Si no, píntalo de rojo.
             - Puedes saber si está a 5 pixeles del objetivo si la diferencia entre la coordeanda x del cuadrado y la x del objetivo es menor a 5 y lo mismo con la y.
     */
+   if (this->shape.getPosition().y + this->shape.getSize().y <= objective.y){
+        this-> speed += 1;
+        this-> shape.move (this->speed);
+    }
+     if (this->shape.getPosition().x + this->shape.getSize().x >= objective.x || this->shape.getPosition().x <0 ){
+        this->shape.setFillColor(Color::Red);
+        this->speed.x *= -1; 
+    }
+    if (this->shape.getPosition().y + this->shape.getSize().y >= objective.y || this->shape.getPosition().y < 0 ){
+        this->shape.setFillColor(Color::Green);
+        this->speed.y *= -1; 
+        this-> shape.setPosition(this->shape.getPosition().x, 600-this->shape.getSize().y);
+    }
 }
 
 void Rectangle::setObjective(Vector2f objective)
 {
+    float dirx, diry, magnitud;
     /**(5pts)
         Asigna objetivo a la propiedad objective.
         Calcula la dirección hacia el objetivo y la velocidad necesaria para llegar al objetivo:
@@ -53,6 +77,12 @@ void Rectangle::setObjective(Vector2f objective)
     /**Reto de valientes (0.5 décimas extra)
         Haz que el centro del cuadrado se posicione donde se dio click en lugar de que la esquina superior izquierda se posicione donde se dio click.
     */
+   this->objective=Vector2f(objective);
+   dirx=abs(this->objective.x-this->shape.getPosition().x);
+   diry=abs(this->objective.y-this->shape.getPosition().y);
+   magnitud=sqrt(pow(dirx,2)+pow(diry,2));
+   this->speed.x=(dirx/magnitud)*10.f;
+   this->speed.y=(diry/magnitud)*10.f;
 }
 
 void Rectangle::drawTo(RenderWindow &window)
--- main.cpp
+++ main.cpp
@@ -4,7 +4,26 @@
 #include <iostream>
 using namespace sf;
 using namespace std;
+/*
 
+- Debe haber un rectángulo en pantalla en todo momento.
+- Cuando se de click en la pantalla, el cuadrado se deberá mover a una velocidad razonable hacía donde se dio click.
+- Usa la base de código que se proporciona y responde todos los comentarios multilínea que haya (1 en main.cpp y 4 en Rectangle.cpp)
+
+
+## Navegar en la terminal
+
+1. Dentro de la terminal usa el comando `ls` para listar los archivos y carpetas que hay dentro de la carpeta en la que estás trabajando.
+2. Una vez identificando la carpeta a la que quieres entrar utiliza el comando `cd nombre-de-la-carpeta` para entrar a la carpeta. Recuerda que después de escribir parte del nombre de la carpeta puedes presionar la tecla Tab para que se autocomplete el nombre.
+3. Una vez dentro de la carpeta de tu proyecto usa el siguiente comando para abrirla en VSCode:
+Para abrirlo en una ventana nueva
+```
+code .
+```
+Para abrirlo en la misma ventana en la que estás trabajando
+```
+code . -r
+*/
 int main()
 {
     // setup
@@ -20,7 +39,14 @@ int main()
         {
             if (event.type == Event::Closed)
                 window.close();
-
+            if (event.type == Event::MouseButtonPressed) 
+            {
+                if (event.mouseButton.button == Mouse::Left)
+                {
+                    rect.setObjective(Mouse::getPosition());
+                    rect.update();
+                }
+            }
             /** (5pts)
                 Cuando se de click izquierdo, se deberá actualizar el objetivo del cuadrado a la posición del mouse.
             */
